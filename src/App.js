4560062fb0a4669daa4d4f063e2af77111a401bc
ubaid Dev <obaidkhanzada2004@gmail.com>
2024-04-13 00:06:09 +0500

    route changing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Quiz} from './Components/Quiz'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Components/Header/Header';
-import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -1,58 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Components/Header/Header';
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-import { Quiz } from './Components/Quiz';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import Home from './Pages/Home';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element:  <App/>,
-//   },
-//   {
-//     path: "/quiz",
-//     element:  <Quiz/>,
-//   }
-// ]);
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      {/* ... etc. */}
-    </Route>,
-      // <Route path='*' element={<h1> Page not Found</h1>}/>
-
-  )
-);
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     {/* <Header/> */}
-//     <App/>
-//     <RouterProvider router={router} /> 
-//     {/* prop ka name always router hoga  eg. router={}*/}
-//   </React.StrictMode>
-// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
